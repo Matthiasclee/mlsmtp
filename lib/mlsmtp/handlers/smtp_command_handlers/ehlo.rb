c9e31bc4302c6344207b57389dc456f831b78d88
Matthias Lee <matthias@matthiasclee.com>
2025-03-11 12:36:39 -0700

    pipelining

--- lib/mlsmtp/handlers/smtp_command_handlers/ehlo.rb
+++ lib/mlsmtp/handlers/smtp_command_handlers/ehlo.rb
@@ -9,6 +9,7 @@ module SMTPServer
         esmtp_message = [
           Config.active["mailname"],
           "SIZE #{Config.active["max_size"]}",
+          "PIPELINING",
         ]
         esmtp_message += [ "8BITMIME", "SMTPUTF8" ] if Config.active["support_8_bit"]
         esmtp_message << "VRFY" unless Config.active["disable_commands"].include?("VRFY")
